78abfaaf2c4b5d9144665a73878627691aa0b320
natthawatthap <natthawatthap@gmail.com>
2025-02-24 14:37:23 +0700

    Refactor ReverseArray function to reverse in place and remove unused code

--- problems/reverse_array.go
+++ problems/reverse_array.go
@@ -1,11 +1,26 @@
 package problems
 
 func ReverseArray(arr []int) []int {
-	reversed := make([]int, len(arr))
+    left, right := 0, len(arr)-1
 
-	for i := 0; i < len(arr); i++ {
-		reversed[i] = arr[len(arr)-1-i]
+	for left < right {
+		// สลับค่าของตำแหน่ง left และ right
+		arr[left], arr[right] = arr[right], arr[left]
+
+		// ขยับ pointer
+		left++
+		right--
 	}
 
-	return reversed
+	return arr
 }
+
+// func ReverseArray(arr []int) []int {
+// 	reversed := make([]int, len(arr))
+
+// 	for i := 0; i < len(arr); i++ {
+// 		reversed[i] = arr[len(arr)-1-i]
+// 	}
+
+// 	return reversed
+// }
